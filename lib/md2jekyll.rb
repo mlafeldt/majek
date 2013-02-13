e42ff55da9e7b992f767b8b8fdb43f71e3b268d1
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-02-13 17:38:44 +0100

    Split up code into bin/lib part #2

--- lib/md2jekyll.rb
+++ lib/md2jekyll.rb
@@ -52,3 +52,30 @@ module MdInc
     end
   end
 end
+
+module Md2Jekyll
+  class Application
+    MATCH_TITLE = %r{^# (.+)$}
+
+    def initialize(argv)
+      @filename = argv.first
+      @base_dir = File.dirname(File.expand_path(@filename))
+    end
+
+    def run
+      markdown = File.read(@filename)
+      title = markdown.match(MATCH_TITLE)[1]
+      jekyll_body = markdown.gsub(MATCH_TITLE, '')
+      jekyll_header = %Q(---
+layout: post
+title: "#{title}"
+date: #{Time.now}
+comments: true
+categories:
+---)
+      MdInc::Commands::root(@base_dir)
+      tp = MdInc::TextProcessor.new
+      puts tp.process(jekyll_header + jekyll_body)
+    end
+  end
+end
